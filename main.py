dd84fa96431d34efe0f9ad2525e9fa20110c62b2
hibiki <hibiki123123@gmail.com>
2021-02-03 22:31:38 +0900

    add slime class.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame as pg
+import pygame
 
 from handle_image import resize
 from set import SCREEN_WIDTH, SCREEN_HEIGHT, BRAVER_IMG_PATH, SLIME_IMG_PATH, FIELD_IMG_PATH
@@ -13,42 +13,42 @@ def put_to_screen(img, x, y):
     screen.blit(img, (x, y))
 
 
-pg.init()
+pygame.init()
 
-screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pg.display.set_caption('Dragon quest monster')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Dragon quest monster')
 
-icon = pg.image.load(SLIME_IMG_PATH)
-pg.display.set_icon(icon)
+icon = pygame.image.load(SLIME_IMG_PATH)
+pygame.display.set_icon(icon)
 
-bg_img = pg.image.load(FIELD_IMG_PATH)
+bg_img = pygame.image.load(FIELD_IMG_PATH)
 
 automatic_set_img_size(BRAVER_IMG_PATH, screen.get_height(), screen.get_width())
-braver_img = pg.image.load(BRAVER_IMG_PATH)
+braver_img = pygame.image.load(BRAVER_IMG_PATH)
 
 automatic_set_img_size(SLIME_IMG_PATH, screen.get_height(), screen.get_width())
-enemy_img = pg.image.load(SLIME_IMG_PATH)
+enemy_img = pygame.image.load(SLIME_IMG_PATH)
 
 running: bool = True
 while running:
     screen.fill((0, 0, 0))
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
         # i don't understand how to keypress
-        if event.type == pg.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             print('key press')
-            if event.key == pg.K_UP:
+            if event.key == pygame.K_UP:
                 print('KEYUP')
-            if event.key == pg.K_DOWN:
+            if event.key == pygame.K_DOWN:
                 print('KEYDOWN')
-            if event.key == pg.K_LEFT:
+            if event.key == pygame.K_LEFT:
                 print('KEYLEFT')
-            if event.key == pg.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 print('KEYRIGHT')
 
     put_to_screen(bg_img, 0, 0)
     put_to_screen(braver_img, 300, 400)
     put_to_screen(enemy_img, 500, 700)
-    pg.display.update()
+    pygame.display.update()
--- monster/monster.py
+++ monster/monster.py
@@ -1,2 +1,8 @@
 class Monster:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+
+class Slime(Monster):
+    def __init__(self, name):
+        super().__init__(name)
