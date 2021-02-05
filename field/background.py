578f5a774ed0ed211e9e47403c6ebb292a6b725e
hibiki <hibiki123123@gmail.com>
2021-02-05 19:07:04 +0900

    function add press key right -> change background to combat. class add background class

--- field/background.py
+++ field/background.py
@@ -1,2 +1,8 @@
+from handle_image import resize
+import pygame
+
 class BackGround(object):
-    pass
+    def __init__(self, img_path: str, screen_h: int, screen_w: int):
+        self.img_path = img_path
+        resize(self.img_path, screen_h, screen_w)
+        self.pg_img = pygame.image.load(self.img_path)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from allies.human import Braver
 from enemy.monster import Slime
 from handle_image import resize
+from field.background import BackGround
 from set import SCREEN_WIDTH, SCREEN_HEIGHT, BRAVER_IMG_PATH, SLIME_IMG_PATH, FIELD_IMG_PATH, COMBAT_IMG_PATH
 
 
@@ -17,10 +18,8 @@ pygame.display.set_caption('Dragon quest monster')
 
 icon = pygame.image.load(SLIME_IMG_PATH)
 pygame.display.set_icon(icon)
-print('master')
-print('fff')
-resize(FIELD_IMG_PATH, screen.get_height(), screen.get_width())
-bg_img = pygame.image.load(FIELD_IMG_PATH)
+
+field = BackGround(FIELD_IMG_PATH, screen.get_height(), screen.get_width())
 
 braver = Braver('mike', BRAVER_IMG_PATH, screen.get_height(), screen.get_width())
 
@@ -29,7 +28,7 @@ slime = Slime('kororo', SLIME_IMG_PATH, screen.get_height(), screen.get_width())
 is_running: bool = True
 while is_running:
     screen.fill((0, 0, 0))
-    bg = bg_img
+    back_ground = field.pg_img
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
@@ -43,11 +42,10 @@ while is_running:
                 print('key left')
             if event.key == pygame.K_RIGHT:
                 print('key right')
-                resize(COMBAT_IMG_PATH, screen.get_height(), screen.get_width())
-                bg_img2 = pygame.image.load(COMBAT_IMG_PATH)
-                bg = bg_img2
+                combat = BackGround(COMBAT_IMG_PATH, screen.get_height(), screen.get_width() )
+                back_ground = combat.pg_img
 
-        put_to_screen(bg_img, 0, 0)
+        put_to_screen(back_ground, 0, 0)
         put_to_screen(braver.pg_img, 300, 400)
         put_to_screen(slime.pg_img, 500, 700)
         pygame.display.update()
